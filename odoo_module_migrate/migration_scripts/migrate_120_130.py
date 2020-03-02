c31fcb3c0dab3f77ecca7bda546c352032938016
Katherine Zaoral <kz@adhoc.com.ar>
2020-03-02 15:23:55 -0300

    [ADD] version_functions: be able to run version proper fuctions at file content level

--- odoo_module_migrate/migration_scripts/migrate_120_130.py
+++ odoo_module_migrate/migration_scripts/migrate_120_130.py
@@ -1,6 +1,7 @@
 # Copyright (C) 2019 - Today: GRAP (http://www.grap.coop)
 # @author: Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+import re
 
 _TEXT_ERRORS = {
     "*": {
@@ -30,3 +31,20 @@ _TEXT_REPLACES = {
         r"( |\t)*<field name=('|\")view_type('|\")>.*</field>\n": "",
     }
 }
+
+
+def find_compute_with_context(**kwargs):
+    """ Return the name of the compute methods that depends on context """
+    logger = kwargs['logger']
+    res = []
+    regex = r"(def compute|def _compute)(.*?)(def |@api\.)"
+    matches = re.finditer(regex, kwargs['content'], re.MULTILINE | re.DOTALL)
+    for matchNum, match in enumerate(matches, start=1):
+        if 'context.get' in match.group(2) or 'with_context' in match.group(2):
+            res.append((match.group(1) + match.group(2)).split('(')[0].replace('def ', ''))
+    for item in res:
+        logger.warning("[13] %s: add @api.depends_context() in compute method", item)
+
+
+_VERSION_FUNCTIONS = [
+    find_compute_with_context]
